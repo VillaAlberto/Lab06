61160797bf0a512dfb83f55dda696340ae871396
Alberto <s245184@studenti.polito.it>
2020-04-25 17:40:14 +0200

    Funziona iterazione, non condizione numero 1

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -11,26 +11,25 @@ public class Model {
 	private final static int NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN = 3;
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
-	private MeteoDAO DAO = new MeteoDAO();
+
 	private List<Rilevamento> bestSoluzione;
 	private int bestPunteggio;
-	int counter = 0;
+
+	private MeteoDAO DAO;
 
 	public Model() {
+		DAO = new MeteoDAO();
 		bestSoluzione = null;
 		bestPunteggio = 2000;
-
 	}
 
 	// of course you can change the String output with what you think works best
 	public String getUmiditaMedia(int mese) {
 		String result = "";
-
 		for (String localita : DAO.getAllLocalita()) {
 			double media = calcolaMedia(DAO.getAllRilevamentiLocalitaMese(mese, localita));
-			result += String.format("%s %30.2f%%\n", localita, media);
+			result += String.format("%-10s %5.2f%%\n", localita, media);
 		}
-
 		return result;
 	}
 
@@ -40,23 +39,22 @@ public class Model {
 			somma += r.getUmidita();
 		}
 		return somma / allRilevamentiLocalitaMese.size();
-
 	}
 
 	// of course you can change the String output with what you think works best
 	public String trovaSequenza(int mese) {
 
+		// Per ogni città ho i suoi 15 rilevamenti
 		List<Citta> listaCitta = new LinkedList<Citta>();
-		List<Rilevamento> parziale = new LinkedList<Rilevamento>();
-		int livello = 0;
-		// Ottengo una lista con tutte le città e per ognuna NUMERO_GIORNI_TOTALI di
-		// rilevazioni
 		for (String localita : DAO.getAllLocalita()) {
-			listaCitta.add(
-					new Citta(localita, DAO.getQuindiciRilevamentiLocalitaMese(mese, localita, NUMERO_GIORNI_TOTALI)));
+			listaCitta.add(new Citta(localita, DAO.getQuindiciRilevamentiLocalitaMese(mese, localita, NUMERO_GIORNI_TOTALI)));
 		}
 
+		List<Rilevamento> parziale = new LinkedList<Rilevamento>();
+		int livello = 0;
+
 		ricorsiva(livello, parziale, listaCitta);
+
 		String result = "";
 		for (Rilevamento r : bestSoluzione) {
 			result += r.getLocalita() + "\n";
@@ -67,26 +65,15 @@ public class Model {
 
 	private void ricorsiva(int livello, List<Rilevamento> parziale, List<Citta> listaCitta) {
 
+		// casi terminali
 		for (Citta c : listaCitta) {
-			if (c.getCounter() > NUMERO_GIORNI_CITTA_MAX) {
+			if (c.getCounter() > NUMERO_GIORNI_CITTA_MAX)
 				return;
-			}
 		}
 
-		int contatore = 0;
-		for (Rilevamento r : parziale) {
-			if (parziale.indexOf(r)>1&&r.getLocalita().compareTo(parziale.get(parziale.indexOf(r)-1).getLocalita()) != 0) {
-				contatore = 0;
-			} else {
-				contatore++;
-			}
-			if (contatore == NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN) {
-				return;
-			}
-		}
-
-		if (livello == NUMERO_GIORNI_TOTALI) {
-			System.out.println(counter++);
+		// condizione terminale
+		if (livello == NUMERO_GIORNI_TOTALI /*&& AlmenoGiorniMin(parziale)*/) {
+			// migliore della bestSoluzione?
 			int costo = calcolaCosto(parziale);
 			if (costo < bestPunteggio) {
 				bestSoluzione = new LinkedList<Rilevamento>(parziale);
@@ -95,26 +82,46 @@ public class Model {
 			return;
 		}
 
+		// generiamo i sotto-problemi
 		for (Citta c : listaCitta) {
 			parziale.add(c.getRilevamenti().get(livello));
 			c.increaseCounter();
 			ricorsiva(livello + 1, parziale, listaCitta);
-			// backtracking
-
 			parziale.remove(parziale.size() - 1);
 			c.setCounter(c.getCounter() - 1);
 		}
+
+	}
+
+	private boolean AlmenoGiorniMin(List<Rilevamento> parziale) {
+		// Se ne trova 1 o 2 da soli rimanda false
+
+		for (int i = 0; i < NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN; i++) {
+			if (parziale.get(0).getLocalita().compareTo(parziale.get(i).getLocalita()) != 0)
+				return false;
+		}
+		
+		for (int i=NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN; i<NUMERO_GIORNI_TOTALI-1; i++)
+		{
+			if (!parziale.get(i+1).equals(parziale.get(i)))
+			{
+				if (!parziale.get(i).equals(parziale.get(i-1))||!parziale.get(i).equals(parziale.get(i-2)))
+					return false;
+			}
+		}
+
+		return true;
 	}
 
 	private int calcolaCosto(List<Rilevamento> parziale) {
 		int costo = 0;
 		for (Rilevamento r : parziale) {
 			costo += r.getUmidita();
-			if (parziale.indexOf(r)>1&&r.getLocalita().compareTo(parziale.get(parziale.indexOf(r)-1).getLocalita()) != 0) {
+			if (parziale.indexOf(r) > 1
+					&& r.getLocalita().compareTo(parziale.get(parziale.indexOf(r) - 1).getLocalita()) != 0) {
 				costo += COST;
 			}
 		}
-
 		return costo;
 	}
 
